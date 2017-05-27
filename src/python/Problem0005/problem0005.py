c2082657bc510a7a7bdc06b2b68734ba5eedf0b6
Iain Sproat <iainsproat@gmail.com>
2017-05-27 20:27:17 +0400

    Completed 0005
    Verified result

--- src/python/Problem0005/problem0005.py
+++ src/python/Problem0005/problem0005.py
@@ -1,13 +1,13 @@
 def smallest_positive_multiple(min_range, max_range):
     i = 1
-    limit = 100000 #prevent infinite loops!
+    limit = 1000000000 #prevent infinite loops!
     candidate = 0
     while i < limit:
         candidate = i * max_range
-        print("testing candidate {0}".format(candidate))
+        #print("testing candidate {0}".format(candidate))
         success = True
         for j in range(max_range - 1, min_range, -1):
-            print("testing candidate against {0}".format(j))
+            #print("testing candidate against {0}".format(j))
             if candidate % j != 0:
                 success = False
                 i += 1 #next candidate
@@ -19,4 +19,4 @@ def smallest_positive_multiple(min_range, max_range):
     return candidate
 
 if __name__ == "__main__":
-    print(smallest_positive_multiple(1, 10))
+    print(smallest_positive_multiple(1, 20))
